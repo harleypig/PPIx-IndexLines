e31a7d1dee59c004728bfcac128912bb8bf55d02
Harley Pig <harleypig@gmail.com>
2010-11-29 00:15:53 -0700

    nearly done

--- lib/PPIx/IndexLines.pm
+++ lib/PPIx/IndexLines.pm
@@ -1,4 +1,5 @@
 package PPIx::IndexLines;
+
 # ABSTRACT: Given a line number, returns some basic information about where in the perl document you are
 
 =head1 NAME
@@ -9,11 +10,54 @@ PPIx::IndexLines
 
 =cut
 
+# VERSION
+
+=head1 SYNOPSIS
+
+  #!/usr/bin/perl
+  use strict;
+  use warnings;
+  use PPIx::IndexLines;
+  my $document = PPIx::IndexLines->new( +shift );
+  $document->index_lines;
+  print $document->line_type( +shift );
+
+=cut
+
 use strict;
 
 use PPI;
 
-sub new { PPI::Document->new( $_[1] ) }
+=head1 METHODS
+
+=head2 new
+
+Accepts either a scalar or a scalar reference.  This is passed directly to
+PPI::Document::new, so the same rules as in that method apply.
+
+Basically, if you pass in a scalar, it will be assumed to be a filename and
+said file will be loaded.
+
+Otherwise, a scalar reference is assumed to be a PPI document and will be
+parsed directly.
+
+=head2 index_lines
+
+Call this method to index the lines for the new PPI::Document.
+
+=head2 line_type
+
+Call this method with a line number, it will return one of
+
+  BEGIN CHECK UNITCHECK INIT END POD __DATA__ __END__
+
+or
+
+  <PackageName>::<SubName>
+
+=cut
+
+sub new { PPI::Document->new( $_[ 1 ] ) }
 
 package PPI::Document;
 
@@ -32,14 +76,14 @@ sub index_lines {
 
     'PPI::Statement::Data'      => sub { '__DATA__' },
     'PPI::Statement::End'       => sub { '__END__' },
-    'PPI::Statement::Package'   => sub { $package = $_[0]->namespace },
-    'PPI::Statement::Scheduled' => sub { $_[0]->type },
-    'PPI::Statement::Sub'       => sub { sprintf '%s::%s', $package, $_[0]->name },
+    'PPI::Statement::Package'   => sub { $package = $_[ 0 ]->namespace },
+    'PPI::Statement::Scheduled' => sub { $_[ 0 ]->type },
+    'PPI::Statement::Sub'       => sub { sprintf '%s::%s', $package, $_[ 0 ]->name },
 
     # Is this interesting enough to include?
     #'PPI::Token::Comment'       => sub { sprintf '%s (%s)', $package, 'comment' },
 
-    'PPI::Token::Pod'           => sub { 'POD' },
+    'PPI::Token::Pod' => sub { 'POD' },
 
   );
 
@@ -51,8 +95,7 @@ sub index_lines {
 
     next if exists $self->{ 'line' }{ $line_number };
 
-    $self->{ 'line' }{ $_ } = $info
-      for $last_line_number+1 .. $line_number-1;
+    $self->{ 'line' }{ $_ } = $info for $last_line_number + 1 .. $line_number - 1;
 
     $last_line_number = $line_number;
 
@@ -61,8 +104,8 @@ sub index_lines {
     $self->{ 'line' }{ $line_number } = $info = exists $lookup{ $node_type } ? $lookup{ $node_type }->( $e ) : $package;
 
   }
-}
+} ## end sub index_lines
 
-sub line_type { $_[0]->{ 'line' } || '' }
+sub line_type { $_[ 0 ]->{ 'line' } || '' }
 
 1;
